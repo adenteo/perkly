27df37af14b0fcaaf2ba1d92845ee378032d2d01
Aden Teo <88359484+adenteo@users.noreply.github.com>
2024-11-16 00:56:13 +0800

    Set up privy route

--- app/api/privy/route.ts
+++ app/api/privy/route.ts
@@ -2,17 +2,27 @@ import prisma from "@/lib/db";
 export async function POST(req: Request) {
   try {
     const body = await req.json();
-    if (body.type === "user.wallet_created") {
-      await prisma.user.create({
-        data: {
-          id: body.user.id,
-          email: body.user.linked_accounts[0].email,
-          username: body.user.linked_accounts[0].email.split("@")[0],
-          walletAddress: body.user.wallet.address,
-        },
-      });
-      return Response.json({ status: 201 });
+    if (body.wallet.type !== "smart_wallet") {
+      return Response.json({ error: "Invalid wallet type" }, { status: 400 });
     }
+    let email = "";
+    body.user.linked_accounts.forEach((account: any) => {
+      if ("email" in account) {
+        email = account.email;
+      }
+    });
+    if (!email) {
+      return Response.json({ error: "Email not found" }, { status: 400 });
+    }
+    const user = await prisma.user.create({
+      data: {
+        id: body.user.id,
+        email,
+        username: email.split("@")[0],
+        walletAddress: body.wallet.address,
+      },
+    });
+    return Response.json({ status: 201 });
   } catch (error) {
     console.error("Error creating user:", error);
     return Response.json({ error: "Internal server error" }, { status: 500 });
