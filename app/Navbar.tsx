20cc57af1a4476a7606436dfaf9a64d5bf9e9776
Eugene Tay <tayyjeugene@gmail.com>
2024-11-17 01:49:55 +0700

    multipleir

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -3,15 +3,19 @@ import { Button } from "@/components/ui/button";
 import { usePrivy } from "@privy-io/react-auth";
 import { useSmartWallets } from "@privy-io/react-auth/smart-wallets";
 import { HandCoins } from "lucide-react";
+import { SquareStack } from "lucide-react";
 import Image from "next/image";
 import { useBalance } from "wagmi";
 import Perkly from "../assets/Perkly.svg";
 import NotificationStream from "./components/notification-stream";
 import { useRouter } from "next/navigation";
-
+import useMultiBaas from "./hooks/use-multibaas";
+import { useState, useEffect } from "react";
 const Navbar = () => {
   const { client } = useSmartWallets();
   const router = useRouter();
+  const { getMultiplier } = useMultiBaas();
+  const [multiplier, setMultiplier] = useState<number>(0);
 
   const perklyToken = useBalance({
     address: client?.account.address,
@@ -23,6 +27,21 @@ const Navbar = () => {
 
   const disableLogin = !ready || (ready && authenticated);
   const disableLogout = !ready || (ready && !authenticated);
+  // Function to fetch the multiplier
+  const fetchMultiplier = async () => {
+    try {
+      const result = await getMultiplier(); // Call your asynchronous function here
+      console.log(result);
+      setMultiplier(result ?? 0);
+    } catch (error) {
+      console.error("Failed to fetch multiplier:", error);
+    }
+  };
+
+  // Fetch the multiplier when the component mounts
+  useEffect(() => {
+    fetchMultiplier();
+  }, []);
 
   return (
     <div className="flex justify-between items-center">
@@ -57,6 +76,13 @@ const Navbar = () => {
           >
             <HandCoins className="inline" />
             {perklyToken.data?.formatted || 0}
+          </p>
+          <p className="rounded-lg p-2 flex justify-center items-center font-semibold">
+            <p className="text-sm ml-2">
+              x
+              <strong className="text-lg">{multiplier !== null ? multiplier : "Loading..."}</strong>{" "}
+              Token multiplier
+            </p>
           </p>
           <Button disabled={disableLogout} onClick={logout}>
             Log out
